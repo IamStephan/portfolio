284a580111507123d5b84b8d6921db010220dea2
iamstephan <stephanburger54@gmail.com>
2021-12-21 11:25:40 +0200

    heading component

---
+++ src/components/heading/index.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import clsx from "clsx";
+
+type TFontSIzes = "xl" | "lg" | "md" | "sm" | "xs";
+type TElements = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+
+interface Props extends React.HTMLProps<HTMLHeadingElement> {
+  font?: TFontSIzes;
+  as?: TElements;
+}
+
+/**
+ * NOTE:
+ * ======
+ * Tailwind already has a non linear scale for font sizes
+ * (Something between Major and Minor third scaling: https://type-scale.com/)
+ * So having the type scale linearly in numeric values based on the breakpoints
+ * yields a concise scaling system.
+ *
+ * TODO: Maybe add a xl sixing variant?
+ *
+ */
+const FontSizes: Record<TFontSIzes, string> = {
+  xl: "text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl",
+  lg: "text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl",
+  md: "text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl",
+  sm: "text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl",
+  xs: "text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl",
+};
+
+const Heading: React.FC<Props> = ({
+  as = "h1",
+  font = "lg",
+  children,
+  className,
+  ...rest
+}) => {
+  const Element = as;
+  const fontSize = FontSizes[font];
+
+  return (
+    <Element
+      className={clsx("font-bold font-heading", fontSize, className)}
+      {...rest}
+    >
+      {children}
+    </Element>
+  );
+};
+
+export default Heading;

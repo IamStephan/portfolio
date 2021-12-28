0e546caca2bd8ab3cf18ba0b29a9945240fc5464
iamstephan <stephanburger54@gmail.com>
2021-12-28 14:59:31 +0200

    Internal link component

--- src/components/link/index.tsx
+++ src/components/link/index.tsx
@@ -1,36 +1,14 @@
 import React from 'react'
 import NextLink from 'next/link'
-import { useRouter } from 'next/router'
-import clsx from 'clsx'
-
-import { matchURL } from '@/utils/matchURL'
 
 export interface Props extends React.HTMLProps<HTMLAnchorElement> {
-  className?: string
-  activeClassName?: string
-  activePath?: string | Array<string>
   to: string
 }
 
-const Link: React.FC<Props> = ({
-  children,
-  className,
-  activeClassName,
-  activePath,
-  to,
-  ...rest
-}) => {
-  const { asPath } = useRouter()
-  const pathToMatch = activePath || to
-
+const Link: React.FC<Props> = ({ className, to, children, ...rest }) => {
   return (
     <NextLink href={to}>
-      <a
-        className={clsx(className, {
-          [activeClassName || '']: matchURL(pathToMatch, asPath),
-        })}
-        {...rest}
-      >
+      <a className={className} {...rest}>
         {children}
       </a>
     </NextLink>

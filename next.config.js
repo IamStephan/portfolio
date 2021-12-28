733e7972191e7ee7976bf45a5097ac24400d54b9
iamstephan <stephanburger54@gmail.com>
2021-12-28 14:56:56 +0200

    support for importing svgs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find(
+      (rule) => rule.test && rule.test.test(".svg")
+    );
+    fileLoaderRule.exclude = /\.svg$/;
+    config.module.rules.push({
+      test: /\.svg$/,
+      loader: require.resolve("@svgr/webpack"),
+    });
+    return config;
+  },
+};

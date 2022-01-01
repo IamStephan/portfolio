0f7bc271519dc62da8036e6843ac7b8ac117b61a
iamstephan <stephanburger54@gmail.com>
2022-01-01 22:18:26 +0200

    forwarding ref and exporting constants

--- src/templates/section/index.tsx
+++ src/templates/section/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 import clsx from 'clsx'
 
 type TWitdthClamp = 'lg' | 'md' | 'sm' | 'none'
@@ -11,50 +11,59 @@ interface Props extends React.HTMLProps<HTMLDivElement> {
   sectionGap?: TSectionGapMode
 }
 
-const WidthClampMap: Record<TWitdthClamp, string> = {
+export const WidthClampMap: Record<TWitdthClamp, string> = {
   lg: 'max-w-screen-3xl',
   md: 'max-w-screen-2xl',
   sm: 'max-w-screen-xl',
   none: '',
 }
 
+export const PaddedStyles = 'px-6 md:px-10 lg:px-14'
+export const MarginStyles = 'mx-6 md:mx-10 lg:mx-14'
+
 // NOTE: Beware of margin collapsing
-const SectionGapMap: Record<TSectionGapMode, string> = {
+export const SectionGapMap: Record<TSectionGapMode, string> = {
   both: 'my-20 md:my-32 lg:my-40',
   top: 'mt-20 md:mt-32 lg:mt-40',
   bottom: 'mb-20 md:mb-32 lg:mb-40',
   none: '',
 }
 
-const SectionTemplate: React.FC<Props> = ({
-  as = 'section',
-  className,
-  widthClamp = 'lg',
-  isPadded,
-  children,
-  sectionGap = 'both',
-  ...rest
-}) => {
-  const Element = as
-  const _withClamp = WidthClampMap[widthClamp]
-  const _sectionGap = SectionGapMap[sectionGap]
+const SectionTemplate = forwardRef<HTMLDivElement, Props>(
+  (
+    {
+      as = 'section',
+      className,
+      widthClamp = 'lg',
+      isPadded,
+      children,
+      sectionGap = 'both',
+      ...rest
+    },
+    ref
+  ) => {
+    const Element = as
+    const _withClamp = WidthClampMap[widthClamp]
+    const _sectionGap = SectionGapMap[sectionGap]
 
-  return (
-    <Element
-      className={clsx(
-        'mx-auto',
-        {
-          'px-6 md:px-10 lg:px-14': isPadded,
-        },
-        _withClamp,
-        _sectionGap,
-        className
-      )}
-      {...rest}
-    >
-      {children}
-    </Element>
-  )
-}
+    return (
+      <Element
+        className={clsx(
+          'mx-auto',
+          {
+            [PaddedStyles]: isPadded,
+          },
+          _withClamp,
+          _sectionGap,
+          className
+        )}
+        ref={ref}
+        {...rest}
+      >
+        {children}
+      </Element>
+    )
+  }
+)
 
 export default SectionTemplate

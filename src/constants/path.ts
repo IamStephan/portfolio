213d1a00bdd7953c3d3e299b03c5d19503ac5707
iamstephan <stephanburger54@gmail.com>
2022-01-01 22:16:58 +0200

    path/dir constants

---
+++ src/constants/path.ts
@@ -0,0 +1,8 @@
+import path from 'path'
+
+const ROOT_DIR = process.cwd()
+const CASE_STUDIES_DIR = path.resolve(ROOT_DIR, './content')
+
+const CONTENT_DIR_PREFIX = 'case-studies'
+
+export { ROOT_DIR, CASE_STUDIES_DIR, CONTENT_DIR_PREFIX }

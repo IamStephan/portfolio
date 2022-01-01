eb6dafc14ad6db26f49f20e072b190365cb6bb04
iamstephan <stephanburger54@gmail.com>
2022-01-01 22:18:49 +0200

    hook to check if route is active

--- src/hooks/useIsRouteActive.ts
+++ src/hooks/useIsRouteActive.ts
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router'
-import { matchURL } from '@/utils/matchURL'
+import { matchURL } from '@/utils/match_url'
 
 const useIsRouteActive = (path: string | Array<string>) => {
   const { asPath } = useRouter()

d8323ed31452bc97741e8456635367429c6295d4
iamstephan <stephanburger54@gmail.com>
2022-01-04 21:45:43 +0200

    Logo chip

---
+++ src/components/logo_chip/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import clsx from 'clsx'
+
+import { Logos } from '@/components/logos'
+
+interface Props extends React.HTMLProps<HTMLDivElement> {
+  logo: keyof typeof Logos
+}
+
+const LogoChip: React.FC<Props> = ({ className, logo }) => {
+  const Logo = Logos[logo].element
+  const alias = Logos[logo].alias
+
+  return (
+    <div
+      className={clsx(
+        'relative px-3 pr-4 py-1 text-sm md:text-base font-medium bg-gray-900 flex space-x-3 items-center border border-gray-100',
+        className
+      )}
+    >
+      <Logo className="w-auto h-4" />
+
+      <span className="block">{alias}</span>
+    </div>
+  )
+}
+
+// TODO: Implement Component
+export default LogoChip

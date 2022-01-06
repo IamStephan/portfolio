fd3503918b0c31ac00c36bec69695dcc77345734
iamstephan <stephanburger54@gmail.com>
2022-01-06 22:43:08 +0200

    properly hash image content (WIP)

---
+++ lib/shared-utils/imageHash.ts
@@ -0,0 +1,27 @@
+/**
+ * Reference: https://github.com/pwlmaciejewski/imghash
+ */
+
+import { imageFromBuffer, getImageData } from '@canvas/image'
+import { bmvbhash } from 'blockhash-core'
+
+export const imageHash = async (
+  imageBuffer: Buffer,
+  filename: string,
+  extension: string
+) => {
+  let imageData: ImageData
+
+  try {
+    const image = await imageFromBuffer(imageBuffer)
+    imageData = getImageData(image) as ImageData
+  } catch (e) {
+    if (extension === 'jpeg' || extension === 'jpg') {
+      throw new Error('Jpeg error')
+    } else {
+      throw new Error(`${e}`)
+    }
+  }
+
+  return `${filename}-${bmvbhash(imageData, 8)}.${extension}`
+}

d80284428a5e75f5736fdb8651234079d618d0d1
iamstephan <stephanburger54@gmail.com>
2022-01-06 22:42:53 +0200

    removed fold hasing algo (not reliable)

--- lib/shared-utils/fileHashing.ts
+++
@@ -1,6 +0,0 @@
-import objectHash from 'object-hash'
-
-export const objectBasedHash = (data: Object, length: number = 8): string => {
-  const hash = objectHash(data)
-  return hash.substr(0, length)
-}

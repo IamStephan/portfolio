d82a4f276a9b5a54cb1a808917024599dcab01ea
iamstephan <stephanburger54@gmail.com>
2022-01-10 13:23:29 +0200

    extracted container classes into a function

--- src/components/clickable/index.tsx
+++ src/components/clickable/index.tsx
@@ -11,51 +11,61 @@ interface BaseProps {
   isJumbo?: boolean
   rightIcon?: React.ReactNode
   leftIcon?: React.ReactNode
+  disabled?: boolean
 }
 
 type Props = BaseProps & ClickableProps
 
+const _containerStyles = (
+  type: TButtonType,
+  isDisabled: boolean,
+  hasRightIcon: boolean,
+  classNames?: string
+) => {
+  const base =
+    'relative inline-flex items-center py-3 overflow-hidden font-semibold transition-all duration-150 ease-in-out text-gray-50 group cursor-pointer'
+  const iconBaseStyles = hasRightIcon
+    ? 'pr-14 pl-6 hover:pr-6 hover:pl-14'
+    : 'px-6'
+
+  switch (type) {
+    case 'primary': {
+      const primaryStyles =
+        'bg-persian-red-main hover:bg-persian-red-main/80 active:bg-persian-red-main/60'
+
+      return clsx(base, iconBaseStyles, primaryStyles, classNames)
+    }
+
+    case 'secondary': {
+      const secondaryStyles = 'hover:bg-gray-800 active:bg-gray-700'
+      const disabledStyles =
+        isDisabled && 'bg-gray-800 cursor-not-allowed opacity-60'
+      return clsx(
+        base,
+        iconBaseStyles,
+        secondaryStyles,
+        disabledStyles,
+        classNames
+      )
+    }
+  }
+}
+
 const Button: React.FC<Props> = ({
   type = 'primary',
-  isJumbo,
+  isJumbo = false,
   className,
-  disabled,
+  disabled = false,
   rightIcon,
-  // Force second icon to show i there is an icon
   leftIcon = rightIcon,
   children,
   ...rest
 }) => {
   return (
     <Clickable
-      className={clsx(
-        'relative inline-flex items-center py-3 overflow-hidden font-semibold transition-all duration-150 ease-in-out text-gray-50 group cursor-pointer',
-        {
-          'pr-14 pl-6 hover:pr-6 hover:pl-14': !!rightIcon,
-          'px-6': !rightIcon,
-          'bg-persian-red-main hover:bg-persian-red-main/80':
-            type === 'primary',
-          'hover:bg-gray-800': type === 'secondary',
-        },
-        className
-      )}
+      className={_containerStyles(type, disabled, !!rightIcon, className)}
       {...rest}
     >
-      {/* Right icon */}
-      {!!rightIcon && (
-        <span
-          className={clsx(
-            'absolute w-5 duration-200 ease-out right-6 group-hover:translate-x-11',
-            {
-              'w-6 group-hover:translate-x-12': isJumbo,
-            }
-          )}
-        >
-          {rightIcon}
-        </span>
-      )}
-
-      {/* left Icon */}
       {!!leftIcon && (
         <span
           className={clsx(
@@ -69,14 +79,27 @@ const Button: React.FC<Props> = ({
         </span>
       )}
 
-      {/* Text */}
       <span
         className={clsx({
-          'text-lg px-2 py-1': isJumbo,
+          'md:text-lg px-1.5 py-2': isJumbo,
+          'text-base px-1 py-0': !isJumbo,
         })}
       >
         {children}
       </span>
+
+      {!!rightIcon && (
+        <span
+          className={clsx(
+            'absolute w-5 duration-200 ease-out right-6 group-hover:translate-x-11',
+            {
+              'w-6 group-hover:translate-x-12': isJumbo,
+            }
+          )}
+        >
+          {rightIcon}
+        </span>
+      )}
 
       {/* Borders */}
       {type !== 'primary' && (
@@ -86,7 +109,5 @@ const Button: React.FC<Props> = ({
   )
 }
 
-// TODO: Implement active, focus states
-// TODO: Implement disabled state
-// TODO: Cleanup Component
+//* Done
 export default Button

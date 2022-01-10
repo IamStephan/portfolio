1b8b3e9e1187df66ea457806cd3fe38d189d02e4
iamstephan <stephanburger54@gmail.com>
2022-01-10 13:28:36 +0200

    content column section

---
+++ src/sections/content_column/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import clsx from 'clsx'
+
+import Section from '@/templates/section'
+import Heading from '@/components/heading'
+
+export interface Props {
+  title: string | React.ReactNode
+  proseContent?: boolean
+}
+
+const ProseStyles =
+  'prose !prose-invert prose-neutral prose-inner prose-sm md:prose-base'
+
+const ContentColumn: React.FC<Props> = ({
+  title,
+  children,
+  proseContent = true,
+}) => {
+  return (
+    <Section
+      widthClamp="md"
+      isPadded
+      className="flex flex-col space-y-2 sm:space-y-4 lg:space-y-0 lg:space-x-8 xl:space-x-16 lg:flex-row"
+    >
+      <div className="lg:max-w-[30%] xl:max-w-[40%] w-full">
+        {typeof title === 'string' ? (
+          <Heading font="md" as="h2" className="sticky top-10">
+            {title}
+          </Heading>
+        ) : (
+          title
+        )}
+      </div>
+      <div
+        className={clsx('flex-1 max-w-3xl justify-self-end w-full self-end', {
+          [ProseStyles]: proseContent,
+        })}
+      >
+        {children}
+      </div>
+    </Section>
+  )
+}
+
+//* Done
+export default ContentColumn

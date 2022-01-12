acadcceaac9c6efe7abfd6ced2cfebe0adfd58b6
iamstephan <stephanburger54@gmail.com>
2022-01-12 12:23:40 +0200

    logo store

---
+++ src/stores/logo.ts
@@ -0,0 +1,17 @@
+import { proxy, useSnapshot } from 'valtio'
+
+export type TLogoPos = 'in-loader' | 'in-header' | 'in-mobile-menu'
+
+interface IStore {
+  logoPos: TLogoPos
+}
+
+const store = proxy<IStore>({
+  logoPos: 'in-loader',
+})
+
+export const setLogoPos = (pos: TLogoPos) => (store.logoPos = pos)
+
+export const useLogoStore = () => useSnapshot(store)
+
+export default store

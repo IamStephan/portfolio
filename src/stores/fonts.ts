79e68375d2034fc02437d6024c5b6f5c38a59fe4
iamstephan <stephanburger54@gmail.com>
2022-01-12 12:23:30 +0200

    fonts store

---
+++ src/stores/fonts.ts
@@ -0,0 +1,52 @@
+import { proxy } from 'valtio'
+import FontFaceObserver from 'fontfaceobserver'
+
+interface IStore {
+  isFontsLoaded: boolean
+}
+
+interface IFontFace {
+  family: string
+  weight?:
+    | `light`
+    | `normal`
+    | `bold`
+    | `bolder`
+    | `100`
+    | `200`
+    | `300`
+    | `400`
+    | `500`
+    | `600`
+    | `700`
+    | `800`
+    | `900`
+  style?: `normal` | `italic` | `oblique`
+  stretch?:
+    | `normal`
+    | `ultra-condensed`
+    | `extra-condensed`
+    | `condensed`
+    | `semi-condensed`
+    | `semi-expanded`
+    | `expanded`
+    | `extra-expanded`
+    | `ultra-expanded`
+}
+
+export const waitForFont = async (fonts: Array<IFontFace>, timeout: number) => {
+  const promises = fonts.map(({ family, weight, style, stretch }) =>
+    new FontFaceObserver(family, {
+      weight,
+      style,
+      stretch,
+    }).load(undefined, timeout)
+  )
+
+  // Resolve regardless of font loaded state
+  await Promise.allSettled(promises)
+}
+
+const store = proxy<IStore>({ isFontsLoaded: false })
+
+export default store

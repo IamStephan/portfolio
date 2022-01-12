5dc52d6aeb0a8ad6ffb3b6d3d97d517795d09fd4
iamstephan <stephanburger54@gmail.com>
2022-01-12 12:24:53 +0200

    personal logo

---
+++ src/components/p_logo/_logo_base.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import clsx from 'clsx'
+import { Flipped } from 'react-flip-toolkit'
+import { FlippedProps, FlipId } from 'flip-toolkit/lib/types'
+
+export interface Props extends Omit<FlippedProps, 'children'> {
+  className?: string
+  divRef?: React.LegacyRef<HTMLDivElement>
+  flipPrefix?: string
+}
+
+const getFlipID = (flipId?: FlipId, flipPrefix?: string) => {
+  if (!flipId && !flipPrefix) return undefined
+  if (!flipPrefix) return flipId
+  return `${flipPrefix}-${flipId}`
+}
+
+const LogoBase: React.FC<Props> = ({
+  className,
+  shouldFlip = () => true,
+  divRef,
+  flipId,
+  flipPrefix,
+  children,
+  ...rest
+}) => {
+  return (
+    <Flipped
+      flipId={getFlipID(flipId, flipPrefix)}
+      shouldFlip={shouldFlip}
+      {...rest}
+    >
+      <div ref={divRef} className={clsx('w-full h-full', className)}>
+        {children}
+      </div>
+    </Flipped>
+  )
+}
+
+export default LogoBase
---
+++ src/components/p_logo/index.tsx
@@ -0,0 +1,68 @@
+import React, { useCallback } from 'react'
+import clsx from 'clsx'
+
+import Letter from './letter'
+import Wings from './wings'
+import Ribbon from './ribbon'
+
+type TType = 'main' | 'mini' | 'nano'
+
+const shouldLogoFlip = (prev: any, cur: any) => prev.asPath === cur.asPath
+
+interface Props extends React.HTMLProps<HTMLDivElement> {
+  type: TType
+  shouldAnimate?: boolean
+  wrapperClassname?: string
+  flipPrefix?: string
+  shouldExit?: boolean
+}
+
+const Icon: React.FC<Props> = ({
+  type,
+  className,
+  wrapperClassname,
+  shouldAnimate = true,
+  flipPrefix,
+  shouldExit = true,
+  ...rest
+}) => {
+  const shouldFlip = useCallback(
+    (prev: Object, cur: Object) => {
+      if (!shouldAnimate) return false
+
+      return shouldLogoFlip(prev, cur)
+    },
+    [shouldAnimate]
+  )
+
+  return (
+    <div className={clsx('relative', className)} {...rest}>
+      <div className={clsx('relative', wrapperClassname)}>
+        <Letter
+          shouldFlip={shouldFlip}
+          flipPrefix={flipPrefix}
+          className={clsx({
+            'h-full w-auto top-0 left-1/2 -translate-x-1/2 absolute':
+              type === 'mini' || type === 'main',
+          })}
+        />
+
+        {type !== 'nano' && (
+          <Wings flipPrefix={flipPrefix} shouldFlip={shouldFlip} />
+        )}
+      </div>
+
+      {type === 'main' && (
+        <Ribbon
+          flipPrefix={flipPrefix}
+          shouldExit={shouldExit}
+          shouldFlip={shouldFlip}
+        />
+      )}
+    </div>
+  )
+}
+
+// TODO: Better handling of exit animations
+// TODO: Rather use images or redo ribbon component to decrease size
+export default Icon

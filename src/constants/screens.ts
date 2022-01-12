670273129966130bc20f4a032e3feb46c4eb178a
iamstephan <stephanburger54@gmail.com>
2022-01-12 12:22:13 +0200

    screen constants

---
+++ src/constants/screens.ts
@@ -0,0 +1,16 @@
+const Screens = {
+  xs: '480px',
+  sm: '640px',
+  md: '768px',
+  lg: '1024px',
+  xl: '1280px',
+  '2xl': '1536px',
+  '3xl': '1920px',
+}
+
+/**
+ * NOTE:
+ * ========
+ * Copied and pasted from `./lib/tailwindcss-presets-screens`
+ */
+export default Screens

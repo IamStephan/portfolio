ce185bd51b68390e39bab8f19bc66d00484d4c83
iamstephan <stephanburger54@gmail.com>
2022-01-12 12:22:25 +0200

    socials constant

---
+++ src/constants/socials.ts
@@ -0,0 +1,23 @@
+import LinkedInLogo from '@/assets/social_logos/linkedin.svg'
+import GithubLogo from '@/assets/social_logos/github.svg'
+import InstaLogo from '@/assets/social_logos/instagram.svg'
+
+const Socials = [
+  {
+    name: 'LinkedIn',
+    Logo: LinkedInLogo,
+    url: 'https://linkedin.com/in/iamstephann',
+  },
+  {
+    name: 'Github',
+    Logo: GithubLogo,
+    url: 'https://github.com/IamStephan',
+  },
+  {
+    name: 'Instagram',
+    Logo: InstaLogo,
+    url: 'https://instagram.com/i_am_stephann/',
+  },
+]
+
+export default Socials

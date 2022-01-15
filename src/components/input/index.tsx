b4f5d4fca55719b392ca0a6b01056a37962d9a33
iamstephan <stephanburger54@gmail.com>
2022-01-15 14:15:02 +0200

    simplified input

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,75 +1,91 @@
-import React, { useState, useCallback } from 'react'
+import React, { useState, useCallback, forwardRef } from 'react'
 import clsx from 'clsx'
 
-import InputContainer, {
-  Props as InputProps,
-} from '@/components/input_container'
+import InfoIcon from '@/assets/icons/information-line.svg'
 
-interface BaseProps {
+interface Props extends React.HTMLProps<HTMLElement> {
   containerClassName?: string
+  as?: 'text' | 'area'
+  error?: string
 }
 
-type Props = BaseProps & InputProps
-
-const Input: React.FC<Props> = ({
-  className,
-  containerClassName,
-  onFocus,
-  onBlur,
-  ...rest
-}) => {
-  const [isFocused, setIsFocused] = useState(false)
-
-  const _handleOnFocus = useCallback(
-    (e) => {
-      setIsFocused(true)
-
-      if (typeof onFocus === 'function') {
-        onFocus(e)
-      }
+const Input = forwardRef<HTMLElement, Props>(
+  (
+    {
+      className,
+      containerClassName,
+      onFocus,
+      onBlur,
+      as = 'text',
+      error,
+      ...rest
     },
-    [setIsFocused, onFocus]
-  )
+    ref
+  ) => {
+    const [isFocused, setIsFocused] = useState(false)
+    const Element = as === 'text' ? 'input' : 'textarea'
 
-  const _handleOnBlur = useCallback(
-    (e) => {
-      setIsFocused(false)
+    const _handleOnFocus = useCallback(
+      (e) => {
+        setIsFocused(true)
 
-      if (typeof onBlur === 'function') {
-        onBlur(e)
-      }
-    },
-    [setIsFocused, onBlur]
-  )
+        if (typeof onFocus === 'function') {
+          onFocus(e)
+        }
+      },
+      [setIsFocused, onFocus]
+    )
 
-  return (
-    <div
-      className={clsx('relative flex flex-col space-y-1.5', containerClassName)}
-    >
-      <div className="relative">
-        <InputContainer
-          onFocus={_handleOnFocus}
-          onBlur={_handleOnBlur}
-          type="text"
-          className={clsx(
-            'bg-gray-800 border-none focus:border-none focus:ring-0 block w-full py-3 px-5',
-            className
-          )}
-          {...rest}
-        />
-        <div
-          className={clsx(
-            'transform -translate-x-1/2 duration-150 left-1/2 -translate-y-1/2 absolute bottom-0 h-0.5 bg-gray-400',
-            {
-              'w-full': isFocused,
-              'w-0': !isFocused,
-            }
-          )}
-        />
+    const _handleOnBlur = useCallback(
+      (e) => {
+        setIsFocused(false)
+
+        if (typeof onBlur === 'function') {
+          onBlur(e)
+        }
+      },
+      [setIsFocused, onBlur]
+    )
+
+    return (
+      <div
+        className={clsx(
+          'relative flex flex-col space-y-1.5',
+          containerClassName
+        )}
+      >
+        <div className="relative">
+          <Element
+            onFocus={_handleOnFocus}
+            onBlur={_handleOnBlur}
+            type="text"
+            className={clsx(
+              'bg-gray-800 border-none focus:border-none focus:ring-0 block w-full py-3 px-5',
+              className
+            )}
+            ref={ref as any}
+            {...rest}
+          />
+
+          <div
+            className={clsx(
+              'transform -translate-x-1/2 duration-150 left-1/2 -translate-y-1/2 absolute bottom-0 h-0.5 bg-gray-400',
+              {
+                'w-full': isFocused,
+                'w-0': !isFocused,
+              }
+            )}
+          />
+        </div>
+        {error && (
+          <div className="flex items-center space-x-1.5 text-sm text-persian-red-500">
+            <InfoIcon className="w-5 h-5" />
+            <span>{error}</span>
+          </div>
+        )}
       </div>
-    </div>
-  )
-}
+    )
+  }
+)
 
-//* Done
 export default Input

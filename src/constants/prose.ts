648c71f5fd12a35647855a632904993e078a6d1d
iamstephan <stephanburger54@gmail.com>
2022-01-15 22:53:40 +0200

    include heading styles

--- src/constants/prose.ts
+++ src/constants/prose.ts
@@ -1,4 +1,9 @@
-const ProseStyles =
-  'prose !prose-invert prose-neutral prose-inner prose-sm md:prose-base'
+const HeadingStyles = `
+prose-h3:font-bold prose-h3:text-lg sm:prose-h3:text-xl md:prose-h3:text-2xl lg:prose-h3:text-3xl xl:prose-h3:text-4xl
+prose-h4:font-bold prose-h3:prose-h4:text-base sm:prose-h4:text-lg md:prose-h4:text-xl lg:prose-h4:text-2xl xl:prose-h4:text-3xl
+prose-h5:font-bold prose-h5:text-base md:prose-h5:text-lg xl:prose-h5:text-xl
+`
+
+const ProseStyles = `prose !prose-invert prose-neutral prose-sm md:prose-base ${HeadingStyles}`
 
 export default ProseStyles

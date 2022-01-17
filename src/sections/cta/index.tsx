180f4ca4e40036bd0fb98539b08f421695a835a7
iamstephan <stephanburger54@gmail.com>
2022-01-17 11:17:11 +0200

    cta section

---
+++ src/sections/cta/index.tsx
@@ -0,0 +1,49 @@
+import React, { Fragment } from 'react'
+
+import Section from '@/templates/section'
+import Heading from '@/components/heading'
+import Button from '@/components/clickable'
+
+import ResumeIcon from '@/assets/icons/profile-line.svg'
+import DownloadIcon from '@/assets/icons/download-2-line.svg'
+
+interface Props {
+  title?: string | React.ReactNode
+}
+
+const DefaultTitle = (
+  <span>
+    Think we're a good fit? Let's get <i>talking</i>.
+  </span>
+)
+
+const CallToAction: React.FC<Props> = ({ title = DefaultTitle }) => {
+  return (
+    <Section
+      sectionGap="none"
+      isPadded
+      className="flex flex-col items-center justify-center py-16 space-y-6 md:space-y-10 lg:space-y-14 sm:py-24 md:py-32 lg:py-40 bg-dark-500 bg-opacity-30"
+    >
+      <Heading as="h2" className="max-w-5xl text-center">
+        {Array.isArray(title)
+          ? title.map((line, i) => (
+              <Fragment key={`${line}_${i}`}>
+                {line} <br />
+              </Fragment>
+            ))
+          : title}
+      </Heading>
+      <Button
+        as="externalLink"
+        isJumbo
+        rightIcon={<ResumeIcon />}
+        leftIcon={<DownloadIcon />}
+      >
+        Download Resumé
+      </Button>
+    </Section>
+  )
+}
+
+//* Done
+export default CallToAction

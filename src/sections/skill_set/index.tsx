0d04bce26c6e4c64ad56e769bffcdc95bd26955b
iamstephan <stephanburger54@gmail.com>
2022-01-17 11:16:58 +0200

    added support for actions

--- src/sections/skill_set/index.tsx
+++ src/sections/skill_set/index.tsx
@@ -1,27 +1,32 @@
 import React from 'react'
 import clsx from 'clsx'
 
-import Section, { PaddedStyles } from '@/templates/section'
+import Section, { PaddedStyles, WidthClampMap } from '@/templates/section'
 import Heading from '@/components/heading'
 import { Logos } from '@/components/logos'
 
 interface Props {
   title: string
+  action?: React.ReactNode
 }
 
-const SkillSet: React.FC<Props> = ({ title }) => {
+const SkillSet: React.FC<Props> = ({ title, action }) => {
   return (
     <Section>
-      <Heading
-        as="h2"
-        font="sm"
+      <div
         className={clsx(
-          'mb-4 text-center sm:mb-6 md:mb-8 lg:mb-12',
+          'flex mx-auto mb-4 sm:mb-6 md:mb-8 lg:mb-12 space-x-6 items-end',
+          WidthClampMap.md,
           PaddedStyles
         )}
       >
-        {title}
-      </Heading>
+        <Heading as="h2" font="sm" className="flex-1 text-center md:text-left">
+          {title}
+        </Heading>
+
+        {action && <div className="hidden md:inline-flex">{action}</div>}
+      </div>
+
       <section className="py-8 bg-[#313131] bg-opacity-20 flex flex-nowrap overflow-hidden justify-center">
         {Array.from({ length: 3 }).map((_, i) => (
           <ul
@@ -37,6 +42,12 @@ const SkillSet: React.FC<Props> = ({ title }) => {
           </ul>
         ))}
       </section>
+
+      {action && (
+        <div className="flex justify-center mt-4 sm:mt-6 md:mt-8 lg:mt-12 md:hidden">
+          {action}
+        </div>
+      )}
     </Section>
   )
 }

1999bfd06da5c1dfd229117d152fa69ac8c0e46d
iamstephan <stephanburger54@gmail.com>
2022-01-17 11:16:25 +0200

    favicon constant

---
+++ src/constants/seo.ts
@@ -0,0 +1,14 @@
+const SeoFavicon = [
+  {
+    rel: 'icon',
+    type: 'image/svg+xml',
+    href: '/icons/favicon.svg',
+  },
+  {
+    rel: 'icon',
+    type: 'image/png',
+    href: '/icons/favicon.png',
+  },
+]
+
+export { SeoFavicon }

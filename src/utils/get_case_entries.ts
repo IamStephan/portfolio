a8b5cbd9329b85868624c2d020168b6830d19ea3
iamstephan <stephanburger54@gmail.com>
2022-01-18 15:35:19 +0200

    don't show template folder in cases

--- src/utils/get_case_entries.ts
+++ src/utils/get_case_entries.ts
@@ -2,6 +2,6 @@ import fg from 'fast-glob'
 
 export const getCaseEntries = async () => {
   return await fg('./content/**/index.{md,mdx}', {
-    // ignore: ['./content/_template/**/*'],
+    ignore: ['./content/_template/**/*'],
   })
 }

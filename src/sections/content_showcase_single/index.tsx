f2c8af6f89ee124eec62fe03c7e4a2935ec33021
iamstephan <stephanburger54@gmail.com>
2022-01-21 10:12:49 +0200

    extracted carousel and zoom logic into hooks

--- src/sections/content_showcase_single/index.tsx
+++ src/sections/content_showcase_single/index.tsx
@@ -1,5 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react'
-import useEmblaCarousel, { EmblaCarouselType } from 'embla-carousel-react'
+import React from 'react'
 import Image, { ImageProps } from 'next/image'
 import { Controlled } from 'react-medium-image-zoom'
 import clsx from 'clsx'
@@ -7,6 +6,8 @@ import clsx from 'clsx'
 import Section, { PaddedStyles, MarginStyles } from '@/templates/section'
 import Heading from '@/components/heading'
 import Button from '@/components/button'
+import useCarousel, { TOptions } from '@/hooks/useCarousel'
+import useCarouselZoom from '@/hooks/useCarouselZoom'
 
 import ArrowLeft from '@/assets/icons/arrow-left-s-line.svg'
 import ArrowRight from '@/assets/icons/arrow-right-s-line.svg'
@@ -17,82 +18,15 @@ interface Props {
   images: Array<ImageProps>
 }
 
-const _handleScrollClick = (
-  direction: 'forward' | 'backward',
-  api?: EmblaCarouselType
-) => {
-  if (!api) return
-  const slides = api.slideNodes().length
-  const currentIndex = api.selectedScrollSnap()
-  let scrollToIndex: number
-
-  switch (direction) {
-    case 'forward': {
-      scrollToIndex = currentIndex === slides - 1 ? 0 : currentIndex + 1
-      break
-    }
-
-    case 'backward': {
-      scrollToIndex = currentIndex === 0 ? slides - 1 : currentIndex - 1
-      break
-    }
-  }
-
-  api.scrollTo(scrollToIndex)
+const CarouselOptions: TOptions = {
+  dragFree: true,
+  containScroll: 'keepSnaps',
+  align: 'center',
 }
 
 const ContentShowcaseSingle: React.FC<Props> = ({ images }) => {
-  const [activeZoom, setActiveZoom] = useState(-1)
-  const [isClickable, setIsClickable] = useState(true)
-
-  const [emblaRef, api] = useEmblaCarousel({
-    dragFree: true,
-    containScroll: 'keepSnaps',
-    align: 'center',
-  })
-
-  const _handleZoomOpen = useCallback(
-    (zoomIndex: number) => () => {
-      if (activeZoom === -1 && isClickable) {
-        setActiveZoom(zoomIndex)
-      }
-    },
-    [activeZoom, isClickable, setActiveZoom]
-  )
-
-  const _handleZoomClose = useCallback(
-    (zoom: boolean) => {
-      if (!zoom) {
-        setActiveZoom(-1)
-      }
-    },
-    [setActiveZoom]
-  )
-
-  const _handleNext = useCallback(() => {
-    _handleScrollClick('forward', api)
-  }, [api])
-
-  const _handlePrev = useCallback(() => {
-    _handleScrollClick('backward', api)
-  }, [api])
-
-  useEffect(() => {
-    if (!api) return
-
-    const _canNotClick = () => setIsClickable(false)
-    const _canClick = () => setIsClickable(true)
-
-    api.reInit()
-
-    api.on('scroll', _canNotClick)
-    api.on('settle', _canClick)
-
-    return () => {
-      api.off('scroll', _canNotClick)
-      api.off('settle', _canClick)
-    }
-  }, [api])
+  const { ref, api, handleNext, handlePrev } = useCarousel(CarouselOptions)
+  const { activeIndex, handleZoomIn, handleZoomOut } = useCarouselZoom(api)
 
   return (
     <Section widthClamp="md">
@@ -114,13 +48,13 @@ const ContentShowcaseSingle: React.FC<Props> = ({ images }) => {
 
         <div className="flex items-center space-x-4 md:space-x-6">
           <Button
-            onClick={_handlePrev}
+            onClick={handlePrev}
             className="flex items-center justify-center p-2 duration-150 rounded-full sm:p-2 ring-1 ring-gray-50 hover:bg-gray-800 active:bg-gray-700"
           >
             <ArrowLeft className="pr-0.5 w-7 h-7" />
           </Button>
           <Button
-            onClick={_handleNext}
+            onClick={handleNext}
             className="flex items-center justify-center p-2 rounded-full md:p-2 ring-1 ring-gray-50 hover:bg-gray-800 active:bg-gray-700"
           >
             <ArrowRight className="pl-0.5 w-7 h-7" />
@@ -129,14 +63,14 @@ const ContentShowcaseSingle: React.FC<Props> = ({ images }) => {
       </div>
 
       {/* Wrapper */}
-      <div ref={emblaRef} className="overflow-hidden bg-dark-900/75">
+      <div ref={ref} className="overflow-hidden bg-dark-900/75">
         {/* container */}
         <div className={clsx('flex mx-44 space-x-12', MarginStyles)}>
           {images.map(({ width, height, ...imageProps }, i) => (
-            <div className="flex-shrink-0" key={i} onClick={_handleZoomOpen(i)}>
+            <div className="flex-shrink-0" key={i} onClick={handleZoomIn(i)}>
               <Controlled
-                isZoomed={i === activeZoom}
-                onZoomChange={_handleZoomClose}
+                isZoomed={i === activeIndex}
+                onZoomChange={handleZoomOut}
                 key={i}
                 zoomMargin={75}
                 overlayBgColorEnd="rgb(12 14 15 / 0.75)"

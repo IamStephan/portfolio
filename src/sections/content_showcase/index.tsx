b01494549f1ccd508068ffa67797c6ea2d851639
iamstephan <stephanburger54@gmail.com>
2022-01-21 10:13:01 +0200

    extracted carousel into hook

--- src/sections/content_showcase/index.tsx
+++ src/sections/content_showcase/index.tsx
@@ -1,7 +1,6 @@
-import React, { useCallback, useEffect } from 'react'
+import React from 'react'
 import Image, { ImageProps } from 'next/image'
 import clsx from 'clsx'
-import useEmblaCarousel, { EmblaCarouselType } from 'embla-carousel-react'
 
 import Section, {
   PaddedStyles,
@@ -10,6 +9,7 @@ import Section, {
 } from '@/templates/section'
 import Heading from '@/components/heading'
 import Button from '@/components/button'
+import useCarousel, { TOptions } from '@/hooks/useCarousel'
 
 import ArrowLeft from '@/assets/icons/arrow-left-s-line.svg'
 import ArrowRight from '@/assets/icons/arrow-right-s-line.svg'
@@ -18,50 +18,14 @@ interface Props {
   images: Array<ImageProps>
 }
 
-const _handleScrollClick = (
-  direction: 'forward' | 'backward',
-  api?: EmblaCarouselType
-) => {
-  if (!api) return
-  const slides = api.slideNodes().length
-  const currentIndex = api.selectedScrollSnap()
-  let scrollToIndex: number
-
-  switch (direction) {
-    case 'forward': {
-      scrollToIndex = currentIndex === slides - 1 ? 0 : currentIndex + 1
-      break
-    }
-
-    case 'backward': {
-      scrollToIndex = currentIndex === 0 ? slides - 1 : currentIndex - 1
-      break
-    }
-  }
-
-  api.scrollTo(scrollToIndex)
+const CarouselOptions: TOptions = {
+  dragFree: true,
+  containScroll: 'keepSnaps',
+  align: 'center',
 }
 
 const ContentShowcase: React.FC<Props> = ({ images }) => {
-  const [emblaRef, api] = useEmblaCarousel({
-    dragFree: true,
-    containScroll: 'keepSnaps',
-    align: 'center',
-  })
-
-  const _handleNext = useCallback(() => {
-    _handleScrollClick('forward', api)
-  }, [api])
-
-  const _handlePrev = useCallback(() => {
-    _handleScrollClick('backward', api)
-  }, [api])
-
-  useEffect(() => {
-    if (!api) return
-
-    api.reInit()
-  }, [api])
+  const { ref, handleNext, handlePrev } = useCarousel(CarouselOptions)
 
   return (
     <Section>
@@ -78,13 +42,13 @@ const ContentShowcase: React.FC<Props> = ({ images }) => {
 
         <div className="flex items-center space-x-4 md:space-x-6">
           <Button
-            onClick={_handlePrev}
+            onClick={handlePrev}
             className="flex items-center justify-center p-2 duration-150 rounded-full sm:p-2 ring-1 ring-gray-50 hover:bg-gray-800 active:bg-gray-700"
           >
             <ArrowLeft className="pr-0.5 w-7 h-7" />
           </Button>
           <Button
-            onClick={_handleNext}
+            onClick={handleNext}
             className="flex items-center justify-center p-2 rounded-full md:p-2 ring-1 ring-gray-50 hover:bg-gray-800 active:bg-gray-700"
           >
             <ArrowRight className="pl-0.5 w-7 h-7" />
@@ -93,7 +57,7 @@ const ContentShowcase: React.FC<Props> = ({ images }) => {
       </div>
 
       {/* Wrapper */}
-      <div ref={emblaRef} className="overflow-hidden">
+      <div ref={ref} className="overflow-hidden">
         {/* container */}
         <div className={clsx('flex mx-64 space-x-12', MarginStyles)}>
           {images.map(({ width, height, ...imageProps }, i) => (

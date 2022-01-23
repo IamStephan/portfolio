f0e4a42123135ec725e5b7649319ce91e555236c
iamstephan <stephanburger54@gmail.com>
2022-01-23 14:16:15 +0200

    case study details page

---
+++ pages/case-studies/[slug].tsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import { GetStaticPaths, GetStaticProps, NextPage } from 'next'
+import { MDXRemoteSerializeResult } from 'next-mdx-remote'
+import { NextSeo } from 'next-seo'
+
+import Page from '@/templates/page'
+import ContentOverview from '@/sections/content_overview'
+import ShowcaseBanner from '@/sections/showcase_banner'
+import MDXContent from '@/sections/mdx_content'
+import CTA from '@/sections/cta'
+import GetSlugs from '@/api/case_studies/get_slugs'
+import GetBySlug from '@/api/case_studies/get_by_slug'
+
+import { IContentFrontmatter } from '@/types/content'
+
+interface Props {
+  frontmatter: IContentFrontmatter
+  source: MDXRemoteSerializeResult
+}
+
+const CaseStudyPage: NextPage<Props> = ({ frontmatter, source }) => {
+  return (
+    <>
+      <NextSeo title={`${frontmatter.title} | Case Study`} />
+      <Page>
+        <ShowcaseBanner
+          title={`[${frontmatter.title}]`}
+          showcase={{
+            alt: `${frontmatter.title} screenshots.`,
+            ...frontmatter.showcase,
+          }}
+        />
+        <ContentOverview
+          overview={frontmatter.overview}
+          role={frontmatter.role}
+          stack={frontmatter.stack}
+          liveLink={frontmatter.links?.liveLink}
+          repo={frontmatter.links?.repo}
+        />
+        <MDXContent {...source} />
+        <CTA />
+      </Page>
+    </>
+  )
+}
+
+export const getStaticProps: GetStaticProps<Props> = async (context) => {
+  const { params } = context
+
+  if (!params?.slug || typeof params.slug !== 'string') {
+    return {
+      notFound: true,
+    }
+  }
+
+  const data = await GetBySlug(params.slug)
+
+  return {
+    props: {
+      ...data,
+    },
+  }
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const slugs = await GetSlugs()
+  const paths = slugs.map((slug) => ({ params: { slug } }))
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+//* Done
+export default CaseStudyPage

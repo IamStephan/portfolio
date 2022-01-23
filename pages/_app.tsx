5ab4d6504b923c3f15994c37a38f520d3c952950
iamstephan <stephanburger54@gmail.com>
2022-01-23 14:07:02 +0200

    layout component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,66 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import React, { useEffect } from 'react'
+import { Flipper, Flipped } from 'react-flip-toolkit'
+import { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+import clsx from 'clsx'
+import { DefaultSeo } from 'next-seo'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import Logo from '@/components/p_logo'
+import Progressbar from '@/components/progressbar'
+import { initialize } from '@/stores/readiness'
+import { useLoaderStore } from '@/stores/loader'
+import { useLogoStore } from '@/stores/logo'
+import { SeoFavicon } from '@/constants/seo'
+import '@/styles/globals.css'
+
+const SpringConfig = {
+  stiffness: 40,
+  damping: 12,
 }
 
-export default MyApp;
+function MyApp({ Component, pageProps }: AppProps) {
+  const { isLoading } = useLoaderStore()
+  const { logoPos } = useLogoStore()
+  const { asPath } = useRouter()
+
+  useEffect(() => {
+    initialize()
+  }, [])
+
+  return (
+    <Flipper
+      flipKey={`${asPath}-${isLoading ? 'hide' : 'ready'}`}
+      spring={SpringConfig}
+      decisionData={{
+        asPath,
+      }}
+    >
+      <DefaultSeo
+        titleTemplate="%s | Frontend Developer"
+        title="Stephan Burger"
+        additionalLinkTags={SeoFavicon}
+      />
+
+      <Progressbar />
+
+      <Component {...pageProps} />
+
+      <Flipped flipId="overlay">
+        <div
+          className={clsx('fixed inset-0 bg-dark-900 pointer-events-none', {
+            '-translate-y-full': !isLoading,
+          })}
+        />
+      </Flipped>
+
+      {logoPos === 'in-loader' && (
+        <div className="fixed inset-0 flex items-center justify-center inset-x-16 xs:inset-8 sm:inset-x-0">
+          <Logo className="w-full h-auto sm:w-96" type="main" />
+        </div>
+      )}
+    </Flipper>
+  )
+}
+
+//* Done
+export default MyApp

477514ba955bbcddce784e9ed8dd08d10c4bd705
iamstephan <stephanburger54@gmail.com>
2022-01-23 17:19:04 +0200

    using chip component as a wrapper

--- src/components/logo_chip/index.tsx
+++ src/components/logo_chip/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import clsx from 'clsx'
 
+import Chip from '@/components/chip'
 import { Logos } from '@/components/logos'
 
 interface Props extends React.HTMLProps<HTMLDivElement> {
@@ -12,16 +13,11 @@ const LogoChip: React.FC<Props> = ({ className, logo }) => {
   const alias = Logos[logo].alias
 
   return (
-    <div
-      className={clsx(
-        'relative px-3 pr-4 py-1 text-sm md:text-base font-medium bg-gray-900 flex space-x-3 items-center border border-gray-100',
-        className
-      )}
-    >
+    <Chip className={clsx('relative', className)} type="secondary">
       <Logo className="w-auto h-4" />
 
       <span className="block">{alias}</span>
-    </div>
+    </Chip>
   )
 }
 

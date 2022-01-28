b2b687904bc94c1b4cf1331d5511093a77396b98
iamstephan <stephanburger54@gmail.com>
2022-01-28 23:00:12 +0200

    added next redirects for local dev, added i18n lang

--- next.config.js
+++ next.config.js
@@ -1,6 +1,33 @@
+function LinkEdits() {
+  return {
+    async rewrites() {
+      return [
+        {
+          source: '/case-studies',
+          destination: '/case-studies/page/1',
+        },
+      ]
+    },
+    async redirects() {
+      return [
+        {
+          source: '/case-studies/page',
+          destination: '/case-studies',
+          permanent: true,
+        },
+      ]
+    },
+  }
+}
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
+  ...LinkEdits(),
   webpack(config) {
     const fileLoaderRule = config.module.rules.find(
       (rule) => rule.test && rule.test.test('.svg')

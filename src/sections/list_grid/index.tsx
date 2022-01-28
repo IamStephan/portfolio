439b260baa8450a27f077f8bf128828f77b79c33
iamstephan <stephanburger54@gmail.com>
2022-01-28 23:02:35 +0200

    extracted grid item

--- src/sections/list_grid/index.tsx
+++ src/sections/list_grid/index.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
-import Image from 'next/image'
-import clsx from 'clsx'
 
 import Section from '@/templates/section'
 import Heading from '@/components/heading'
 import Button from '@/components/clickable'
-import Link from '@/components/link'
 import Pages from '@/constants/pages'
-import { IExtendedFrontmatter } from '@/types/content'
+import Item, { Props as ItemProps } from './item'
 
 import ArrowIcon from '@/assets/icons/arrow-right-line.svg'
 import BriefcaseIcon from '@/assets/icons/briefcase-4-line.svg'
@@ -15,19 +12,7 @@ import BriefcaseIcon from '@/assets/icons/briefcase-4-line.svg'
 interface Props {
   title: string
   collectionTo: string
-  items: Array<IExtendedFrontmatter>
-}
-
-const _shouldExpand = (index: number) => {
-  const position = index + 1
-  const gridCells = 4
-  const modulus = position % gridCells
-
-  if (modulus === 2 || modulus === 3) {
-    return true
-  }
-
-  return false
+  items: Array<Omit<ItemProps, 'index'>>
 }
 
 const ListGrid: React.FC<Props> = ({ title, collectionTo, items }) => {
@@ -50,53 +35,8 @@ const ListGrid: React.FC<Props> = ({ title, collectionTo, items }) => {
       </div>
 
       <div className="grid gap-6 mb-6 sm:mb-8 md:gap-10 sm:grid-cols-2 lg:grid-cols-3 md:mb-0">
-        {items.map(({ slug, showcase, title, to }, i) => (
-          <Link
-            className={clsx(
-              'relative cursor-pointer h-48 sm:h-60 md:h-72 group lg:h-80 xl:h-96',
-              {
-                'lg:col-span-2': _shouldExpand(i),
-              }
-            )}
-            to={to}
-            key={slug}
-          >
-            <Image
-              {...showcase}
-              alt={`${title} showcase on mutliple devices`}
-              // Explicityly setting this to undefined
-              width={undefined}
-              height={undefined}
-              layout="fill"
-              placeholder="blur"
-              objectFit="cover"
-              className="duration-300 group-hover:scale-110"
-            />
-
-            <div
-              className={clsx(
-                'absolute -inset-1 flex items-end justify-between p-4 overflow-hidden duration-300 lg:p-6 bg-gradient-to-t from-dark-900 group-hover:opacity-100',
-                'lg:opacity-0 group-hover:opacity-100'
-              )}
-            >
-              <Heading
-                className={clsx(
-                  'duration-150 line-clamp-1',
-                  'lg:-translate-x-12 lg:opacity-0 group-hover:translate-x-0 group-hover:opacity-100 '
-                )}
-                font="xs"
-                as="h3"
-              >
-                {title}
-              </Heading>
-              <ArrowIcon
-                className={clsx(
-                  'w-auto h-8 duration-150',
-                  'opacity-0 translate-x-12 group-hover:opacity-100 group-hover:translate-x-0'
-                )}
-              />
-            </div>
-          </Link>
+        {items.map((item, i) => (
+          <Item key={i} index={i} {...item} />
         ))}
       </div>
 

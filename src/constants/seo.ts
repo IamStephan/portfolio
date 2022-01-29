8dd9fcfd5e93ec50f49d8e87a018e88ef5b31c53
iamstephan <stephanburger54@gmail.com>
2022-01-29 15:18:51 +0200

    generic seo og images

--- src/constants/seo.ts
+++ src/constants/seo.ts
@@ -11,4 +11,31 @@ const SeoFavicon = [
   },
 ]
 
-export { SeoFavicon }
+const SeoOGImages = [
+  {
+    url: '/og-images/generic/small.jpg',
+    width: 400,
+    height: 400,
+  },
+  {
+    url: '/og-images/generic/medium.jpg',
+    width: 1200,
+    height: 360,
+  },
+  {
+    url: '/og-images/generic/large.jpg',
+    width: 1080,
+    height: 1080,
+  },
+  {
+    url: '/og-images/generic/xlarge.jpg',
+    width: 1080,
+    height: 1920,
+  },
+]
+
+const SeoOG = {
+  images: SeoOGImages,
+}
+
+export { SeoFavicon, SeoOGImages, SeoOG }
